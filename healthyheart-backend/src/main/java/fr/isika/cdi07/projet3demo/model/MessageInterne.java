453756f25fcbe334e75f7a2beb870c6723650d9c
Pierre PM <pierpm@gmail.com>
2021-03-01 23:08:47 +0100

    Derniere version Ok du modele - 20210301_2308

--- healthyheart-backend/src/main/java/fr/isika/cdi07/projet3demo/model/MessageInterne.java
+++ healthyheart-backend/src/main/java/fr/isika/cdi07/projet3demo/model/MessageInterne.java
@@ -10,8 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -43,7 +43,7 @@ public class MessageInterne {
 	@JoinColumn(name="email")
 	private Utilisateur emetteur;
 	
-	@OneToMany
+	@ManyToMany
 	private List<Utilisateur> destinataires;
 	
 	@OneToOne
