3df6ddd1e86155a6634565c84c199ab286a16953
Nora <amarinora65@yahoo.fr>
2021-03-05 15:24:46 +0100

    afficher ajouter modifier utilisateurs

--- projet3demo/healthyheart-services/src/main/java/fr/isika/cdi07/projet3demo/services/UtilisateurService.java
+++ projet3demo/healthyheart-services/src/main/java/fr/isika/cdi07/projet3demo/services/UtilisateurService.java
@@ -2,6 +2,8 @@ package fr.isika.cdi07.projet3demo.services;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,4 +22,27 @@ public class UtilisateurService {
 		return utilisateurRepo.save(utilisateur);
 	}
 
+	public Utilisateur chercherUtilisateurParEmailEtMdp(String email, String mdp) {
+		return utilisateurRepo.findUtilisateurByEmailAndMdp(email, mdp);
+	}
+	
+	public List<Utilisateur> retournerTousLesUtilisateur(){
+		return utilisateurRepo.findAll();
+	}
+	
+	public Utilisateur chercherUtilisateurParEmail(String email) {
+		return utilisateurRepo.findUtilisateurByEmail(email);
+	}
+	
+	public Utilisateur chercherOptionalUtilisateurParEmail(String email) {
+		Optional<Utilisateur> optionalUtilisateur = Optional.of(utilisateurRepo.findUtilisateurByEmail(email));
+		Utilisateur utilisateur = null;
+		if(optionalUtilisateur.isPresent()) {
+			utilisateur = optionalUtilisateur.get();
+		}else {
+			throw new RuntimeException("L'utilisateur n'a pas été trouvé");
+		}
+		return utilisateur;
+	}
+	
 }
