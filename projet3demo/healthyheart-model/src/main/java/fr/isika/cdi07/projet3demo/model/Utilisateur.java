df1e46c83f1ea93a7683d575724718d67fbb9cf0
Nora <amarinora65@yahoo.fr>
2021-03-05 15:24:46 +0100

    afficher ajouter modifier utilisateurs

--- projet3demo/healthyheart-model/src/main/java/fr/isika/cdi07/projet3demo/model/Utilisateur.java
+++ projet3demo/healthyheart-model/src/main/java/fr/isika/cdi07/projet3demo/model/Utilisateur.java
@@ -100,8 +100,6 @@ public class Utilisateur {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
--- projet3demo/healthyheart-presentation/src/main/java/fr/isika/cdi07/projet3demo/controller/UtilisateurController.java
+++ projet3demo/healthyheart-presentation/src/main/java/fr/isika/cdi07/projet3demo/controller/UtilisateurController.java
@@ -1,10 +1,13 @@
 package fr.isika.cdi07.projet3demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.isika.cdi07.projet3demo.model.Projet;
@@ -17,16 +20,61 @@ public class UtilisateurController {
 	@Autowired
 	private UtilisateurService utilisateurService;
 	
-	@GetMapping("/CreerNouveauUtilsateur")
-	public String creerNouveauUtilisateur(Model model) {
+	//Créer Nouvel Utilisateur
+	@GetMapping("/CreerNouvelUtilsateur")
+	public String creerNouvelUtilisateur(Model model) {
 		Utilisateur utilisateur = new Utilisateur();
 		model.addAttribute("utilisateur", utilisateur);
 		return "newUtilisateur";
 	}
+
+	//Enregistrer un utilisateur
 	@PostMapping("/enregistrerUtilisateur")
-	public String enregistrerNouveauUtilisateur(@ModelAttribute("utilisateur") Utilisateur utilisateur) {
+	public String enregistrerNouvelUtilisateur(@ModelAttribute("utilisateur") Utilisateur utilisateur) {
 		utilisateurService.ajouterUtilisateur(utilisateur);
 		return "index";
 	}
+
+	//Pour modifier Un utilisateur 
+	@GetMapping("/showUsersForUpdate/{email}")
+	public String afficherLesUtilisateurPourModification(@PathVariable (value ="email") String email, Model model) {		
+		Utilisateur utilisateur = utilisateurService.chercherOptionalUtilisateurParEmail(email);
+		model.addAttribute("utilisateur", utilisateur);
+		return "update_utilisateur";	
+	}
+	
+	//Afficher Tous les utilisateurs
+	@GetMapping("/showAllUsers")
+	public String afficherTousUtilisateurs(Model model) {
+		model.addAttribute("listeUtilisateurs", utilisateurService.retournerTousLesUtilisateur());
+		return "listeUtilisateurs";
+	}
+	
+	@GetMapping("/ConnecterUtilsateur")
+	public String saisirInformationUtilisateur(Model model) {
+		Utilisateur utilisateur = new Utilisateur();
+		model.addAttribute("utilisateur", utilisateur);
+		return "seConnecter";
+	}
+	
+	//Essayer de connecter un utilisateur
+	@GetMapping("/ConnecterUtilsateur/{email}")
+	public String connecterUtilisateur(@PathVariable (value ="email") String email, Model model) {
+		Utilisateur utilisateur = utilisateurService.chercherOptionalUtilisateurParEmail(email);
+		model.addAttribute("utilisateur", utilisateur);
+		return "utilisateurConnecte";
+	}
+	
+	//@PostMapping("/SeConnecter")
+	//public String connecterUtilisateur(@ModelAttribute("utilisateur") Utilisateur utilisateur) {
+		//Utilisateur utilisateurToUse = null;
+		//Utilisateur utilisateurToUse = utilisateurService.connecterUtilisateur(email, mdp);
+		//utilisateurToUse.getEmail().equals(email);
+		//utilisateurService.connecterUtilisateur(utilisateur.getEmail(), utilisateur.getMdp());
+			
+		//return "utilisateurConnecte";
+	//}	
+	
+	
 	
 }
