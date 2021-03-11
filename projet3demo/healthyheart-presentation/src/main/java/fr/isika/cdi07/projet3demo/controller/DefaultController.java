1c9932a2094db5927d491d5f8efbea7703efc9ba
Arkater <valentin.td.monteiro@gmail.com>
2021-03-11 20:54:40 +0100

    Ajout de la page d'acceuil en front

--- projet3demo/healthyheart-presentation/src/main/java/fr/isika/cdi07/projet3demo/controller/DefaultController.java
+++ projet3demo/healthyheart-presentation/src/main/java/fr/isika/cdi07/projet3demo/controller/DefaultController.java
@@ -4,10 +4,20 @@ import java.util.logging.Logger;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fr.isika.cdi07.projet3demo.model.Utilisateur;
+import fr.isika.cdi07.projet3demo.services.ProjetService;
+import fr.isika.cdi07.projet3demo.services.UtilisateurService;
+
+@Controller
 public class DefaultController {
+	
+	@Autowired
+	private UtilisateurService utilisateurService;
 	
 	private static final Logger LOGGER = Logger.getLogger(MessagerieController.class.getSimpleName());
 	
@@ -18,8 +28,20 @@ public class DefaultController {
 		return "error";
 	}
 	
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String afficherPageAccueil(Model model,HttpSession session) {
+		boolean isConnected = false;
+		Utilisateur monUtilisateur = null;
+		String userEmail = (String) session.getAttribute("emailUtilisateurConnecte");
+		if (userEmail != null) {
+			monUtilisateur = utilisateurService.chercherUtilisateurParEmail(userEmail);
+			isConnected = true;
+			LOGGER.info("Utilisateur connecte : " + monUtilisateur);
+		}
+		
+		model.addAttribute("isConnected", isConnected);
+		model.addAttribute("Utilisateur", monUtilisateur);
+		
 
 		return "index";
 	}
