348fe276946cea2809dc12b9a1e5ff54b3a8b636
oRoKiNoKo <kenshin_levagabond@hotmail.com>
2021-03-12 20:05:03 +0100

    20210312

--- projet3demo/healthyheart-presentation/src/main/resources/static/js/radioButtonTypeDon.js
+++ projet3demo/healthyheart-presentation/src/main/resources/static/js/radioButtonTypeDon.js
@@ -5,4 +5,6 @@ $(document).ready(function(){
         $(".formu").not(targetForm).hide();
         $(targetForm).show();
     });
+	$('input[id=radioMaterielOff]').attr("disabled",true);
+	$('input[id=radioTempsOff]').attr("disabled",true);
 });
